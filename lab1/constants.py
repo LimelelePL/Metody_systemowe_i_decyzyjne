34e5c177b14f3680faa66b8152488a3281499f9c
Dominik <DudekDominik80@gmail.com>
2026-04-02 15:57:45 +0200

    zadanie na 3.0

---
+++ lab1/constants.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+DATA_DIR = Path(__file__).resolve().parent / "resources"
+DATA_FILE = DATA_DIR / "WA_Fn-UseC_-Telco-Customer-Churn.csv"
+
+TENURE = "tenure"
+MONTHLY_CHARGES = "MonthlyCharges"
+TOTAL_CHARGES = "TotalCharges"
+SENIOR_CITIZEN = "SeniorCitizen"
+CHURN = "Churn"
